32fdc26633de32f0b079790d46f198453dd8ef35
Derrick Zhang <dcheung41@gmail.com>
2018-01-09 15:40:00 -0800

    Building Home Page, main app component and added search component, search is able to store value and change its state based onClick

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-
-
+import Search from './components/Search.jsx';
 
 class App extends React.Component {
 
@@ -11,17 +8,24 @@ class App extends React.Component {
   	super(props);
   }
 
-  render() {
-  	return (
-
-  	  <div> Hey this is a rendered React App page 
-
-  	  <p> Hey guys </p>
-  	  </div>
-
-  	)
+  render () {
+    return (
+              <div className="row">
+                <div className="col-lg-12 jumbotron center-text"> 
+                  <img src="http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c513.png" width={400} height={125} mode='fit'/>
+                  <br />
+                  <h2 className="display-4"> Book unique homes and</h2> 
+                  <h2 className="display-4"> experiences all over the world. </h2>
+                  <br />
+                  <br />
+                  <Search />
+                </div>
+              </div>
+          )
   }
 }
+
+<img src="http://sparetimelabs.com/animato/animato/panorama/pan.jpg"/>
 
 
 
