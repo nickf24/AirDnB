696ae4fe8e799f17d59d5ee3a54e1bfc79e825a4
Evan <evantatkinson@gmail.com>
2018-01-09 19:42:51 -0800

    testting heroku

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -53,7 +53,6 @@ class App extends React.Component {
           <Login  handleLoginSubmit={ this.handleLoginSubmit.bind(this) } />
           {this.switchViews()}
         </div>
-
           )
   }
 }
