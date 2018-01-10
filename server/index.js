62247783067df5f7d69b6943a3b968827c50c006
Andrew Kang <andrewsjkang@gmail.com>
2018-01-10 15:03:27 -0800

    Begin integrating Passport authentication

--- server/index.js
+++ server/index.js
@@ -3,15 +3,27 @@ const app = express();
 const path = require('path');
 const parser = require('body-parser');
 
+//// CONFIGURING PASSPORT /////
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+app.use(session({secret: 'flyingMongeese'}));
 
+
+
+app.use(parser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//// Initialize Passport ////
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   // For Evan's homepage
 });
 
-app.post('/', (req, res) => {
-  // default post
+app.post('/login', (req, res) => {
+  console.log('received request: ', req.body);
 });
 
 app.post('/', (req, res) => {
