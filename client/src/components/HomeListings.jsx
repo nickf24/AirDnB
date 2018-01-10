8cf85c275afd19e5481d86e1b71207f6571ab284
Evan <evantatkinson@gmail.com>
2018-01-10 12:09:45 -0800

    added card into HomeListing

--- client/src/components/HomeListings.jsx
+++ client/src/components/HomeListings.jsx
@@ -24,7 +24,21 @@ class HomeListings extends React.Component {
 <div className="container-fluid">
   <div className="row">
     <div className="col-sm-3">
-      One of Four colums
+      
+      <div className="card">
+        <img className="card-img-top" src="https://static.pexels.com/photos/186077/pexels-photo-186077.jpeg" alt="Card image cap"n />
+        <div className="card-body">
+          <h5 className="card-title">$449.99/night</h5>
+          <p className="card-text">4bd 3bth</p>
+          <p className="card-text">Close to Casinos!</p>
+          <p className="card-text">No Monsters under The bed!</p>
+
+          <a href="#" className="btn btn-primary">Reserve</a>
+        </div>
+      </div>
+
+
+
     </div>
     <div className="col-sm-3">
       Two of Four columns
