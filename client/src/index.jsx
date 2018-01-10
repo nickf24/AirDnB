240ad575ba15448350eba08c6accb505c5b16eaf
Evan <evantatkinson@gmail.com>
2018-01-10 11:50:54 -0800

    got ride of test <forms> that were added to checkout heroku

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -48,12 +48,10 @@ class App extends React.Component {
   render () {
 
     return (
-
         <div>
           <Login  handleLoginSubmit={ this.handleLoginSubmit.bind(this) } />
           {this.switchViews()}
         </div>
-
           )
   }
 }
