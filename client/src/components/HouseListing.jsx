80d46bb6b32fd14bbe02eae5c099b9c7e391d2c4
Andrew Kang <andrewsjkang@gmail.com>
2018-01-10 19:25:45 -0800

    Connect ListingView to listings

--- client/src/components/HouseListing.jsx
+++ client/src/components/HouseListing.jsx
@@ -10,7 +10,8 @@ class HouseListing extends React.Component {
   render() {
   	// console.log(this.props.house)
   	return (
-  		<div className ='img-fluid col-md-3 col-sm-6 img-thumbnail thumbnailListings'> 
+			<div className ='img-fluid col-md-3 col-sm-6 img-thumbnail thumbnailListings'
+			     onClick={ this.props.listingClickFn.bind(null, this.props.house) } > 
         <img src = {this.props.house.images[0]} className = "rounded"/>
 	      <h6>{this.props.house.listingTitle}</h6>    
         <p>{this.props.house.price}</p>
