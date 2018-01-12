f58cc0bb1920e7f410190156539b21d4aa3206c1
Derrick Zhang <dcheung41@gmail.com>
2018-01-11 21:12:17 -0800

    adjusted pixels for house listing and postgres is working

--- client/src/components/HouseListing.jsx
+++ client/src/components/HouseListing.jsx
@@ -11,7 +11,7 @@ class HouseListing extends React.Component {
   	return (
 			<div className ='img-responsive center-block col-md-3 col-sm-6 col-xs-12 img-thumbnail thumbnailListings'
 			     onClick={ this.props.listingClickFn.bind(null, this.props.house) } > 
-        <a href="#"><img src = {this.props.house.images[0]} className = "rounded img-thumbnail" width={300} height={200} mode={'fit'}/></a>
+        <a href="#"><img src = {this.props.house.images[0]} className = "rounded img-thumbnail" width={350} height={200} mode={'fit'}/></a>
 	      <h6>{this.props.house.listingtitle}</h6>    
         <p>${this.props.house.price} USD</p>
   		</div> 
