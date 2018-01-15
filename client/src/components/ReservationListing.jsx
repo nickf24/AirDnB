384858066a414dbfe71fb050422d99fabe50ecd4
Derrick Zhang <dcheung41@gmail.com>
2018-01-15 14:22:40 -0800

    reservations

--- client/src/components/ReservationListing.jsx
+++ client/src/components/ReservationListing.jsx
@@ -31,28 +31,21 @@ class ReservationListing extends React.Component {
     var dateToReservation = fromDateObject - Date.parse(isoDate)
     var duration = moment.duration(dateToReservation, 'milliseconds');
 
-    //adds 0 if its a single digit, if not it stays normal.
-    if(duration > 0){
-      var daysTillRes = duration._data.days.toString().length === 1 ? 
-      daysTillRes = '0'+duration._data.days.toString() : 
-      daysTillRes = duration._data.days; 
+    //adds 0 if its a single digit, if not it stays normal. 
+    var daysTillRes = duration._data.days.toString().length === 1 ? 
+                      daysTillRes = '0'+duration._data.days.toString() : 
+                      daysTillRes = duration._data.days;
 
-      var hoursTillRes = duration._data.hours.toString().length === 1 ? 
-      hoursTillRes = '0'+duration._data.hours.toString() : 
-      hoursTillRes = duration._data.hours;
+    var hoursTillRes = duration._data.hours.toString().length === 1 ? 
+                       hoursTillRes = '0'+duration._data.hours.toString() : 
+                       hoursTillRes = duration._data.hours;
 
-    } else {
-      var daysTillRes = 0;
-      var hoursTillRes = 0;
-    }
- 
     var date = new Date();
     var year = date.getFullYear();
-
-    //re arranged reservation date. 
+    
     var fromDate = this.props.house.reserved_dates[0].split('T')[0].slice(5) +'-'+year;
-    var toDate = this.props.house.reserved_dates[1].split('T')[0].slice(5) +'-'+year
-
+    var toDate = this.props.house.reserved_dates[1].split('T')[0].slice(5) +'-'+year;
+    var fromDateObject = Date.parse(this.props.house.reserved_dates[0])
 
     var numberOfRatings = randomNumber(1,10000);
 
@@ -60,10 +53,10 @@ class ReservationListing extends React.Component {
       
 			<div className="row centerDiv formPush col-12">
 
-        <a href="#" className="col-lg-4 col-4 cleanBorder"><img src = {this.props.house.images[0]} 
+        <a href="#" className="col-lg-4 col-4 shadowButton"><img src = {this.props.house.images[0]} 
           className = "img-thumbnail img-responsive " onClick = {() => this.props.handleClick(this.props.house)} width={600} height={400} mode={'fit'}/></a>
 
-        <div className="col-lg-6 col-4 cleanBorder"> 
+        <div className="col-lg-6 col-4 shadowButton"> 
           <h6>
               <div>{this.props.house.typehome} - {this.props.house.bedrooms} {bedroomData} - {this.props.house.bathrooms} {bathroomData}</div>
               {this.props.house.listingtitle}
@@ -80,10 +73,10 @@ class ReservationListing extends React.Component {
           </p>
 
         </div>
-	      <div className="col-lg-2 col-2 centerDiv cleanBorder reservationTimer">
+	      <div className="col-lg-2 col-2 centerDiv shadowButton reservationTimer">
         <br />
           <p className="display-4">{daysTillRes}<h5>&nbsp;days</h5></p>
-          <p className="display-4">{hoursTillRes}<h5>&nbsp;&nbsp;hours</h5></p>
+          <p className="display-4">{hoursTillRes}<h5>&nbsp;hours</h5></p>
         </div>
   		</div> 
   	)
