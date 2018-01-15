20ba76a5cecb5407e457a03bd76ebe9c5513e414
Derrick Zhang <dcheung41@gmail.com>
2018-01-15 13:26:37 -0800

    filled in api key

--- server/config/mapsAPI.js
+++ server/config/mapsAPI.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 
-module.exports.maps_API_KEY = 'PUT KEY HERE';
-=======
 // Put your YouTube API keys here!
 module.exports.maps_API_KEY = 'AIzaSyCJBRsmzb-7GnOaVKkgfEZroolic5xADgk';
->>>>>>> made small changes to reservations/userprofile
+
